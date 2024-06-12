f05216ec1be7b77d45beef96dba5f12c68f9b1c1
dakogiura <dogiura@gmail.com>
2024-06-11 18:27:53 -1000

    Update Views_For_Tableau.sql

--- Views_For_Tableau.sql
+++ Views_For_Tableau.sql
@@ -24,3 +24,37 @@ where continent like 'NULL' and location not like '%income' and location not lik
 group by location;
 select *
 from total_deaths_by_continent;
+
+-- Actual SQL queries used for Tableau Public Dashboard:
+
+-- 1. Global Numbers: Total Cases, Total Deaths, and Death Percentage
+select SUM(new_cases) as total_cases, SUM(new_deaths) as total_deaths, SUM(new_deaths)/SUM(New_Cases)*100 as DeathPercentage
+from covid_deaths
+where location like '%states%' and continent not like 'Null' 
+-- Group By date
+order by 1,2;
+
+-- 2. Total Deaths by Continent
+Select location, SUM(new_deaths) as TotalDeathCount
+From covid_deaths
+-- Where location like '%states%'
+where continent like 'Null' and location not in ('World', 'European Union') and location not like '%income%'
+Group by location
+order by TotalDeathCount desc;
+
+
+-- 3. Percent Population Infected per Country --> Vizualized on a map
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From covid_deaths
+-- Where location like '%states%'
+-- where location not like '%income%'
+Group by Location, Population
+order by PercentPopulationInfected desc;
+
+
+-- 4. Time Series of Percentage of Population Infected by Country --> Selected a few countries to make it easier to view
+Select Location, Population, date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From covid_deaths
+-- Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc;
